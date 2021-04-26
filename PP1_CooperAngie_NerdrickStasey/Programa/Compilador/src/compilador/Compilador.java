b7489dad0455174ecaff6b7e1a43a5d6e86a1da8
Angie Cooper <angcooper18@gmail.com>
2021-04-26 15:23:37 -0600

    Intrucciones en el main para generar el analisis

--- PP1_CooperAngie_NerdrickStasey/Programa/Compilador/src/compilador/Compilador.java
+++ PP1_CooperAngie_NerdrickStasey/Programa/Compilador/src/compilador/Compilador.java
@@ -2,15 +2,89 @@ package compilador;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Compilador {
 
+    public final static int GENERAR = 1;
+    public final static int EJECUTAR = 2;
+    public final static int SALIR = 3;
+
     /**
+     Se crea un menú para generar el analizador léxico y el analizador 
+     sintáctico o ejecutar el programa. 
      * @param args the command line arguments
-     */
+    */
     public static void main(String[] args) {
-        
+        java.util.Scanner in = new Scanner(System.in);
+        int valor = 0;
+        do {
+            System.out.print("-----------------------------------------------\n");
+            System.out.println("Indique una opcion: ");
+            System.out.println("1) Generar el analizador léxico y sintáctico.");
+            System.out.println("2) Ejecutar el programa.");
+            System.out.println("3) Salir.");
+            System.out.print("-----------------------------------------------\n");
+            System.out.print("Opcion: ");
+            valor = in.nextInt();
+            System.out.print("\n");
+            switch (valor) {
+                
+                /* Se genera el analizadr léxico a partir del archivo alexico.flex
+                   Se genera el analizadr sintáctico a partir del archivo asintactico.cup
+                */
+                case GENERAR: {
+                    String archLexico = "";
+                    String archSintactico = "";
+                    if (args.length > 0) {
+                        archLexico = args[0];
+                        archSintactico = args[1];
+                    } else {
+                        archLexico = "alexico.flex";
+                        archSintactico = "asintactico.cup";
+                    }
+                    String[] alexico = {archLexico};
+                    String[] asintactico = {"-parser", "AnalizadorSintactico", archSintactico};
+                    jflex.Main.main(alexico);
+                    try {
+                        java_cup.Main.main(asintactico);
+                    } catch (Exception ex) {
+                        Logger.getLogger(Compilador.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    /*Se mueven los archivos generados a una carpeta Compilador
+                    en src
+                    */
+                    boolean mvAL = moverArch("AnalizadorLexico.java");
+                    boolean mvAS = moverArch("AnalizadorSintactico.java");
+                    boolean mvSym= moverArch("sym.java");
+                    if(mvAL && mvAS && mvSym){
+                        System.exit(0);
+                    }
+                    System.out.println("Programa generado");
+                    break;
+                }
+                case EJECUTAR: {
+                    
+                    /* Se utiliza el archivo de texto test como archivo fuente
+                    del programa
+                    */
+                    String[] archivoPrueba = {"test.txt"};
+                    AnalizadorSintactico.main(archivoPrueba);
+                    break;
+                }
+                case SALIR: {
+                    break;
+                }
+                default: {
+                    System.out.println("Opción inválida!");
+                    break;
+                }
+            }
+        } while (valor != 3);
+
     }
 
     /*
