53f5f5d7f85cc1d5b50c5707307983a212dbf58e
Angie Cooper <angcooper18@gmail.com>
2021-06-01 13:52:43 -0600

    Verificación semántica de funciones y main

--- PP1_CooperAngie_NerdrickStasey/Programa/Compilador/src/compilador/AnalizadorSemantico.java
+++ PP1_CooperAngie_NerdrickStasey/Programa/Compilador/src/compilador/AnalizadorSemantico.java
@@ -13,12 +13,34 @@ import producciones.*;
  */
 public class AnalizadorSemantico {
     private InitProgram program;
+    private int hayErrores = 0;
     
     public AnalizadorSemantico(InitProgram pProgram){
         program = pProgram;
     }
     
-    public boolean Verficar(){
-        
+    public boolean verficar(){
+        if(!mainExisteReturn()) return false;
+        if(!FuncionExisteReturn()) return false;
+        return true;
     }
+    
+    public boolean mainExisteReturn(){
+        return tieneSentenciaReturn(program.getMain().getBlock().getSentences());
+    }
+    
+    public boolean FuncionExisteReturn(){
+        for(Function function : program.getFunctions().getFunctions()){
+            return tieneSentenciaReturn(function.getBlock().getSentences());
+        }
+        return false;
+    }
+    
+    public boolean tieneSentenciaReturn(Sentences sentencesList) {
+        for(Sentence sentence : sentencesList.getSentences()) {
+            if(sentence instanceof Return) return true;
+        }
+        return false;
+    }
+    
 }
